e7b41606e71e0b495eb3685ed0d1a632fb6d398b
Fauzi <fauzi190198@gmail.com>
2018-08-13 22:07:28 +0700

    update

--- mail.php
+++ mail.php
@@ -1,6 +1,10 @@
 <?php
 require 'PHPMailer/PHPMailerAutoload.php';
 $mail = new PHPMailer;
+$name = $_POST['name'];
+$email = $_POST['email'];
+$subject = $_POST['subject'];
+$message = $_POST['message'];
 // Konfigurasi SMTP
 $mail->isSMTP();
 $mail->Host = 'mail.smtp2go.com';
@@ -9,20 +13,19 @@ $mail->Username = 'zikazama';
 $mail->Password = 'FRGwRK3zcUmY';
 $mail->SMTPSecure = 'tls';
 $mail->Port = 587;
-$mail->setFrom('info@contoh.com', 'Codingan');
-$mail->addReplyTo('info@contoh.com', 'Codingan');
+$mail->setFrom($email, $name);
+$mail->addReplyTo($email, $name);
 // Menambahkan penerima
-$mail->addAddress('penerima@contoh.com');
+$mail->addAddress('fauzi190198@gmail.com');
 // Menambahkan cc atau bcc 
 $mail->addCC('cc@contoh.com');
 $mail->addBCC('bcc@contoh.com');
 // Subjek email
-$mail->Subject = 'Kirim Email via SMTP Server di PHP menggunakan PHPMailer';
+$mail->Subject = $subject;
 // Mengatur format email ke HTML
 $mail->isHTML(true);
 // Konten/isi email
-$mailContent = "<h1>Mengirim Email HTML menggunakan SMTP di PHP</h1>
-    <p>Ini adalah email percobaan yang dikirim menggunakan email server SMTP dengan PHPMailer.</p>";
+$mailContent = $message;
 $mail->Body = $mailContent;
 // Kirim email
 if(!$mail->send()){
