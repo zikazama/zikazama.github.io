665aa92d5ac2944a5d1dbdbb0408ada1a61e5542
Fauzi <fauzi190198@gmail.com>
2020-10-30 10:11:55 +0700

    fix kata

--- js/main.js
+++ js/main.js
@@ -79,7 +79,7 @@ $(document).ready(function() {
 
     $(function() {
         typed.typed({
-            strings: ["Fauzi Fadhlur Rohman.", "Web Developer.", "Backend Developer.", "Freelancer.", "Full Stack Develover.", "Mobile Application Developer."],
+            strings: ["Fauzi Fadhlur Rohman.", "Web Developer.", "Backend Developer.", "Freelancer.", "Full Stack Developer.", "Mobile Application Developer."],
             typeSpeed: 100,
             loop: true,
         });
